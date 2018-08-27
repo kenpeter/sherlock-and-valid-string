4408fc946a068729d6d7a416b71090f61be38228
kenpeter <kenpeter@kenpeter>
2018-08-27 13:36:27 +0000

    update

--- sherlock-and-valid-string.js
+++ sherlock-and-valid-string.js
@@ -1,16 +1,20 @@
 
 function isValid(s) {
-
+	// e.g. {a: 2, b: 2, c: 3....} 
     let charMap = {};
+	// char of
     for (const c of s){
+		// prop in
         if (c in charMap){
-            charMap[c]++;
+            charMap[c]++; //
         } else {
-            charMap[c] = 1;
+            charMap[c] = 1; //
         }
     }
 
+	// e.g. {2: how_many_2, 3: how_many_3, ..} 
     let freqMap = {};
+	// prop in 
     for (const k in charMap){
         const freq = charMap[k];
         if (freq in freqMap){
@@ -20,23 +24,54 @@ function isValid(s) {
         }
     }
 
+	// test
+	console.log('--');
+	console.log(charMap);
+	console.log(freqMap);
+	console.log(Object.keys(freqMap));
+	console.log(Object.entries(freqMap));
+
+	// object.keys in array
     if (Object.keys(freqMap).length === 1){
         return 'YES';
     } else if (Object.keys(freqMap).length === 2){
+		// [ [ '2', 7 ], [ '3', 1 ] ]
+		// e.g. 2 is key, 7 is val
         const entries = Object.entries(freqMap);
-        // check if the outlier frequency number only occurs once
-        //      and is exactly one less than the other frequency
-        if (entries[1][1] === 1 && entries[1][0] < entries[0][0]){
+
+		console.log(entries);
+
+		if (
+			parseInt(entries[1][1]) == 1 &&
+			parseInt(entries[0][0]) + 1 == parseInt(entries[1][0])
+		){
             return 'YES';
-        } else if (entries[0][1] === 1 && entries[0][0] < entries[1][0]){
+        } 
+		else if(
+			parseInt(entries[0][1])  == 1 &&
+            parseInt(entries[0][0]) + 1 == parseInt(entries[1][0]) 
+		) {
+			return 'YES';
+		} 
+		else if(
+			parseInt(entries[1][1])  == 1 &&
+            parseInt(entries[0][0]) > parseInt(entries[1][0])	
+		) {
+			return 'YES';
+		}
+		else if(
+            parseInt(entries[0][1])  == 1 &&
+            parseInt(entries[0][0]) < parseInt(entries[1][0])
+        ) {
             return 'YES';
         }
     }
+
     return 'NO';
 }
 
-
-let s = 'abcdefghhgfedecba';
+//let s = 'abcdefghhgfedecba';
+let s = 'ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd';
 let out = isValid(s);
 console.log(out);
 
